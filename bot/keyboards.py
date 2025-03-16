2e523bae1bee49f40e00e8f6e07c17f2c362a0b4
Tatoshechka322 <145149241+Tatoshechka322@users.noreply.github.com>
2025-03-16 15:01:49 +0700

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,1 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+menu_keyboard.add(KeyboardButton("📍 Карта столов"))
+menu_keyboard.add(KeyboardButton("📜 Основные правила заведения"))
+menu_keyboard.add(KeyboardButton("📅 Бронирование столов"))
+menu_keyboard.add(KeyboardButton("⭐ Оставить отзыв"))
+menu_keyboard.add(KeyboardButton("🎉 Специальные события"))
+menu_keyboard.add(KeyboardButton("🎁 Позови друзей"))
