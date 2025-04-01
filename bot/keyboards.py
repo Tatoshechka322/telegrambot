3c58a2d1bf162b3606b51567fbf38365b6e9d916
Tatoshechka322 <145149241+Tatoshechka322@users.noreply.github.com>
2025-04-01 12:37:24 +0700

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,9 +1,36 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-menu_keyboard.add(KeyboardButton("📍 Карта столов"))
-menu_keyboard.add(KeyboardButton("📜 Основные правила заведения"))
-menu_keyboard.add(KeyboardButton("📅 Бронирование столов"))
-menu_keyboard.add(KeyboardButton("⭐ Оставить отзыв"))
-menu_keyboard.add(KeyboardButton("🎉 Специальные события"))
-menu_keyboard.add(KeyboardButton("🎁 Позови друзей"))
+menu_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Основные правила заведения"),
+        ],
+        [
+            KeyboardButton(text="Бронирование столов"),
+            KeyboardButton(text="⭐ Оставить отзыв"),
+        ],
+        [
+            KeyboardButton(text="Специальные события"),
+            KeyboardButton(text="👥 Позови друзей"),
+        ],
+    ],
+    resize_keyboard=True
+)
+
+
+def table_keyboard():
+    """Создает клавиатуру с номерами столов."""
+    table_numbers = [str(i) for i in range(1, 15)]
+    vip_tables = ["vip-1", "vip-2"]
+    all_tables = table_numbers + vip_tables
+    keyboard = []
+    row = []
+    for table in all_tables:
+        row.append(KeyboardButton(text=table))
+        if len(row) == 3:
+            keyboard.append(row)
+            row = []
+    if row:
+        keyboard.append(row)
+
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
